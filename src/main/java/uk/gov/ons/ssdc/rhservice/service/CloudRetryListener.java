e2247a7fc409f3d160d551f55bf2036a7500d5eb
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-06-06 11:10:14 +0100

    Stores to firestore

---
+++ src/main/java/uk/gov/ons/ssdc/rhservice/service/CloudRetryListener.java
@@ -0,0 +1,46 @@
+package uk.gov.ons.ssdc.rhservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.listener.RetryListenerSupport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CloudRetryListener extends RetryListenerSupport {
+
+  private static final Logger log = LoggerFactory.getLogger(CloudRetryListener.class);
+
+  @Override
+  public <T, E extends Throwable> void onError(
+      RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+    Object operationName = context.getAttribute(RetryContext.NAME);
+    //        if (log.isDebugEnabled()) {
+    //            log.debug("{}: Retry failed", operationName);
+    //        }
+    log.warn("Retry failed: " + operationName);
+  }
+
+  @Override
+  public <T, E extends Throwable> void close(
+      RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+
+    // Spring retries have completed. Report on outcome if retries have been used.
+    if (context.getRetryCount() > 0) {
+      Object operationName = context.getAttribute(RetryContext.NAME);
+
+      if (throwable == null) {
+        int numAttempts = context.getRetryCount() + 1; // Add 1 to count the initial attempt
+        //                log.info("{}: Transaction successful after {} attempts", operationName,
+        // numAttempts);
+
+      } else {
+        // On failure the retryCount actually holds the number of attempts
+        int numAttempts = context.getRetryCount();
+        //                log.warn("{}: Transaction failed after {} attempts", operationName,
+        // numAttempts);
+      }
+    }
+  }
+}
