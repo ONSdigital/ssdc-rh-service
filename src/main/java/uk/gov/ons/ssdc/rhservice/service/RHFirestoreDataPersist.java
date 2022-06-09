a89e272bfe4a95ed3594c9b5a8400d60bbff7087
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-06-09 14:43:25 +0100

    comment on spring magic fun

--- src/main/java/uk/gov/ons/ssdc/rhservice/service/RHFirestoreDataPersist.java
+++ src/main/java/uk/gov/ons/ssdc/rhservice/service/RHFirestoreDataPersist.java
@@ -9,6 +9,7 @@ import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import uk.gov.ons.ssdc.rhservice.exceptions.DataStoreContentionException;
 
+//This is in it's own class as Spring Retryable doesn't work otherwise
 @Component
 public class RHFirestoreDataPersist {
 
