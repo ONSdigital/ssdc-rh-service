3f69811175a6fd7a0a9ce5a50172e37ec30ddc23
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-06-10 08:26:05 +0100

    remove more annotations

--- src/main/java/uk/gov/ons/ssdc/rhservice/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ssdc/rhservice/config/AppConfig.java
@@ -10,10 +10,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 public class AppConfig {
 
   @Value("${logging.profile}")
