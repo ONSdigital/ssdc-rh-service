c28a69a7a72f9416619b9559d6eb496aaf0bdeef
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-06-22 08:32:53 +0100

    tweak fix

--- src/test/java/uk/gov/ons/ssdc/rhservice/service/AuthenicatedMessageSenderTest.java
+++ src/test/java/uk/gov/ons/ssdc/rhservice/service/AuthenicatedMessageSenderTest.java
@@ -1,5 +1,18 @@
 package uk.gov.ons.ssdc.rhservice.service;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static uk.gov.ons.ssdc.rhservice.service.AuthenicatedMessageSender.OUTBOUND_EVENT_SCHEMA_VERSION;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -9,58 +22,44 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.ons.ssdc.rhservice.model.dto.EventDTO;
 import uk.gov.ons.ssdc.rhservice.model.dto.EventHeaderDTO;
+import uk.gov.ons.ssdc.rhservice.utils.ObjectMapperFactory;
 import uk.gov.ons.ssdc.rhservice.utils.PubsubHelper;
-
-import java.time.OffsetDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static uk.gov.ons.ssdc.rhservice.service.AuthenicatedMessageSender.OUTBOUND_EVENT_SCHEMA_VERSION;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenicatedMessageSenderTest {
 
-    public static final String TEST_QID = "TEST_QID";
-    public static final String TEST_TOPIC = "Test-Topic";
-    public static final UUID CORRELATION_ID = UUID.randomUUID();
+  public static final String TEST_QID = "TEST_QID";
+  public static final String TEST_TOPIC = "Test-Topic";
+  public static final UUID CORRELATION_ID = UUID.randomUUID();
 
-    @Mock
-    PubsubHelper pubsubHelper;
+  @Mock PubsubHelper pubsubHelper;
 
-    @InjectMocks
-    AuthenicatedMessageSender underTest;
+  @InjectMocks AuthenicatedMessageSender underTest;
 
-    @Test
-    public void testMessageSent() {
-        Map<String, Object> payload = new HashMap<>();
-        payload.put("questionnaire_id", TEST_QID);
-        payload.put("tx_id", CORRELATION_ID);
+  @Test
+  public void testMessageSent() throws JsonProcessingException {
+    Map<String, Object> payload = new HashMap<>();
+    payload.put("questionnaire_id", TEST_QID);
+    payload.put("tx_id", CORRELATION_ID);
 
-        ReflectionTestUtils.setField(underTest, "uacAuthenticationTopic", TEST_TOPIC);
+    ReflectionTestUtils.setField(underTest, "uacAuthenticationTopic", TEST_TOPIC);
 
-        underTest.buildAndSendUacAuthentication(payload);
+    underTest.buildAndSendUacAuthentication(payload);
 
-        ArgumentCaptor<EventDTO> eventArgCaptor = ArgumentCaptor.forClass(EventDTO.class);
-        verify(pubsubHelper).sendMessageToSharedProject(eq(TEST_TOPIC), eventArgCaptor.capture());
+    ArgumentCaptor<String> eventArgCaptor = ArgumentCaptor.forClass(String.class);
+    verify(pubsubHelper).sendMessageToSharedProject(eq(TEST_TOPIC), eventArgCaptor.capture());
 
+    EventDTO eventDTO = ObjectMapperFactory.objectMapper().readValue(eventArgCaptor.getValue(), EventDTO.class);
 
-        EventDTO eventDTO = eventArgCaptor.getValue();
-
-        EventHeaderDTO eventHeaderDTO = eventDTO.getHeader();
-        assertThat(eventHeaderDTO.getVersion()).isEqualTo(OUTBOUND_EVENT_SCHEMA_VERSION);
-        assertThat(eventHeaderDTO.getTopic()).isEqualTo(TEST_TOPIC);
-        assertThat(eventHeaderDTO.getSource()).isEqualTo("RESPONDENT HOME");
-        assertThat(eventHeaderDTO.getChannel()).isEqualTo("RH");
-        assertThat(eventHeaderDTO.getDateTime()).isCloseTo(OffsetDateTime.now(), within(5, ChronoUnit.SECONDS));
-        assertThat(eventHeaderDTO.getMessageId()).isNotNull();
-        assertThat(eventHeaderDTO.getCorrelationId()).isEqualTo(CORRELATION_ID);
-        assertThat(eventHeaderDTO.getOriginatingUser()).isEqualTo("RH");
-
-    }
-
+    EventHeaderDTO eventHeaderDTO = eventDTO.getHeader();
+    assertThat(eventHeaderDTO.getVersion()).isEqualTo(OUTBOUND_EVENT_SCHEMA_VERSION);
+    assertThat(eventHeaderDTO.getTopic()).isEqualTo(TEST_TOPIC);
+    assertThat(eventHeaderDTO.getSource()).isEqualTo("RESPONDENT HOME");
+    assertThat(eventHeaderDTO.getChannel()).isEqualTo("RH");
+    assertThat(eventHeaderDTO.getDateTime())
+        .isCloseTo(OffsetDateTime.now(), within(5, ChronoUnit.SECONDS));
+    assertThat(eventHeaderDTO.getMessageId()).isNotNull();
+    assertThat(eventHeaderDTO.getCorrelationId()).isEqualTo(CORRELATION_ID);
+    assertThat(eventHeaderDTO.getOriginatingUser()).isEqualTo("RH");
+  }
 }
