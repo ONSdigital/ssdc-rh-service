133927356491316f37103ec8fc77b4fd05b2f795
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2022-11-25 10:55:42 +0000

    wip

---
+++ src/main/java/uk/gov/ons/ssdc/rhservice/model/dto/CollectionExerciseUpdateDTO.java
@@ -0,0 +1,16 @@
+package uk.gov.ons.ssdc.rhservice.model.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class CollectionExerciseUpdateDTO {
+  private String collectionExerciseId;
+  //  private String name;
+  //  private UUID surveyId;
+  //  private String reference;
+  //  private OffsetDateTime startDate;
+  //  private OffsetDateTime endDate;
+  //  private Object metadata;
+  private List<CollectionInstrumentSelectionRule> collectionInstrumentSelectionRules;
+}
