b72174fcecd10d9b36fb36631ff745ade302cdb6
AdamHawtin <adam.hawtin@ons.gov.uk>
2024-04-19 09:25:27 +0100

    Fix flaky test

--- src/test/java/uk/gov/ons/ssdc/rhservice/messaging/EqLaunchSenderTest.java
+++ src/test/java/uk/gov/ons/ssdc/rhservice/messaging/EqLaunchSenderTest.java
@@ -36,10 +36,11 @@ class EqLaunchSenderTest {
   @InjectMocks EqLaunchSender underTest;
 
   @Test
-  public void testMessageSent() throws JsonProcessingException {
+  void testMessageSent() throws JsonProcessingException {
     Map<String, Object> payload = new HashMap<>();
     payload.put("qid", TEST_QID);
     payload.put("tx_id", CORRELATION_ID);
+    OffsetDateTime testStart = OffsetDateTime.now();
 
     ReflectionTestUtils.setField(underTest, "eqLaunchTopic", TEST_TOPIC);
 
@@ -56,8 +57,7 @@ class EqLaunchSenderTest {
     assertThat(eventHeaderDTO.getTopic()).isEqualTo(TEST_TOPIC);
     assertThat(eventHeaderDTO.getSource()).isEqualTo("RESPONDENT HOME");
     assertThat(eventHeaderDTO.getChannel()).isEqualTo("RH");
-    assertThat(eventHeaderDTO.getDateTime())
-        .isCloseTo(OffsetDateTime.now(), within(5, ChronoUnit.SECONDS));
+    assertThat(eventHeaderDTO.getDateTime()).isBetween(testStart, OffsetDateTime.now());
     assertThat(eventHeaderDTO.getMessageId()).isNotNull();
     assertThat(eventHeaderDTO.getCorrelationId()).isEqualTo(CORRELATION_ID);
     assertThat(eventHeaderDTO.getOriginatingUser()).isEqualTo("RH");
